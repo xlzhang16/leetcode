2f0a2beaf851b0cf47de11e4b1d5b9ca0aa0df10
Xiulin Zhang <2807774501@qq.com>
2018-07-02 16:29:16 +0800

    Create 448.cpp

---
+++ array/448.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            int t = abs(nums[i]) - 1;
+            nums[t] = -abs(nums[t]);
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0)
+                ans.push_back(i + 1);
+        }
+        return ans;
+    }
+};
